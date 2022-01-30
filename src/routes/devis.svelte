3aa14e0e647e3f8922654f3cc15779e57c1a1cce
anthow <anthonyenglebert@gmail.com>
2022-01-30 14:48:26 +0100

    Initial commit

---
+++ src/routes/devis.svelte
@@ -0,0 +1,30 @@
+
+<header class="bg-green-600 text-white p-2 flex space-around mb-10">
+    <h1 class="text-3xl w-11/12 m-auto"> Administration Sirius</h1>
+    <button class="bg-white text-green-600 rounded p-2"> nouveau devis</button>
+   </header>
+
+   <form class=" w-6/12 m-auto text-center flex flex-col ">
+       <label for="client">Nom du client</label>
+       <input class="border w-1/2 m-auto" name="client" id="client" type="text" />
+       <label>n°TVA client</label>
+       <input class="border w-1/2 m-auto" type="text" />
+       <label>n° BCE client</label>
+       <input class="border w-1/2 m-auto" type="text" />
+       <label>statut juridique</label>
+       <input class="border w-1/2 m-auto" type="text" />
+       <label>titre du devis</label>
+       <input class="border w-1/2 m-auto" type="text" />
+       <label>date de livraison</label>
+       <p>c'est à cette date là que la devis sera envoyé au client</p>
+       <input class="border w-1/2 m-auto" type="date" />
+       <label>montant hors TVA</label>
+       <input class="border w-1/2 m-auto" type="number" />
+       <label>adresse mail client</label>
+       <input class="border w-1/2 m-auto" type="email" />
+       <label>Description</label>
+       <textarea class="border w-9/12 m-auto"  maxlength="500"  rows="10"> </textarea>
+       <input type="submit" value="Envoyer le formulaire" class="mb-10 bg-green-600 w-min text-white p-2 m-auto">
+
+
+   </form>
---
+++ src/routes/index.svelte
@@ -0,0 +1,130 @@
+
+
+<header class="bg-green-600 text-white p-2 flex space-around">
+ <h1 class="text-3xl w-11/12 m-auto"> Administration Sirius Coop</h1>
+ <button class="bg-white text-green-600 rounded p-2"> nouveau devis</button>
+</header>
+<main class="  grid grid-cols-5 gap-x-10 gap-y-10 pt-10  w-11/12 m-auto mb-10	">
+	<article class="bg-red-600 text-white p-4 rounded">
+<h2 class="text-2xl text-center font-black mb-5">Revenus individuels</h2>
+<ul class="text-lg">
+	<li>Laura Laruelle: 0</li>
+	<li>Julie Vanderbyse: 0</li>
+	<li>Anthony Englebert: 0</li>
+	<li>David Hainaut: 0</li>
+	<li>Jeremy Scala: 0</li>
+	<li>Thomas Dussart: 0</li>
+	<li>Christophe Renard: 0</li>
+
+</ul>
+	</article>
+	<article class="bg-blue-600 0 text-white p-4 col-span-3 rounded">
+		<h2 class="text-2xl text-center font-black mb-5">Devis acceptés</h2>
+		<table class=" table-auto m-auto">
+			<thead class="bg-white space-y-4  text-blue-600">
+				<tr>
+					<th>client</th>
+					<th>dénomination de la mission</th>
+					<th>montant HTVA</th>
+					<th>montant TVA</th>
+					<th>montant pour la coop</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th>Interra</th>
+					<th>réalisation de leur site internet</th>
+					<th>3000</th>
+					<th>3000+21%</th>
+					<th>3000-6,5%</th>
+				</tr>
+			</tbody>
+
+		</table>
+
+	</article>
+	<article class="bg-red-600 text-white p-4 rounded">
+		<h2 class="text-2xl text-center font-black mb-5">Chiffre d'affaire 2022</h2>
+		<h3 class="text-2xl text-center font-black mb-5">0 €</h3>
+
+		
+			</article>
+
+			<article class="bg-blue-600 0 text-white p-4 col-span-3 rounded">
+				<h2 class="text-2xl text-center font-black mb-5">Devis en attente de validation</h2>
+				<table class=" table-auto m-auto">
+					<thead class="bg-white space-y-4  text-blue-600">
+						<tr>
+							<th>client</th>
+							<th>dénomination de la mission</th>
+							<th>montant HTVA</th>
+							<th>montant TVA</th>
+							<th>montant pour la coop</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th>Interra</th>
+							<th>réalisation de leur site internet</th>
+							<th>3000</th>
+							<th>3000+21%</th>
+							<th>3000-6,5%</th>
+						</tr>
+					</tbody>
+		
+				</table>
+		
+			</article>
+
+			<article class="bg-blue-600 0 text-white p-4 col-span-2 rounded">
+				<h2 class="text-2xl text-center font-black mb-5">contrat en attente de paiement  </h2>
+				<table class=" table-auto m-auto">
+					<thead class="bg-white space-y-4  text-blue-600">
+						<tr>
+							<th>Coopérateur</th>
+							<th>montant </th>
+							<th>date de demande de paiement</th>
+							<th>statut </th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th>Laura</th>
+							<th>1250€</th>
+							<th>12/01/2022</th>
+							<th>En cours</th>
+						</tr>
+					</tbody>
+		
+				</table>
+		
+			</article>
+
+		<article class="bg-blue-600 0 text-white p-4 col-span-3 rounded">
+			<h2 class="text-2xl text-center font-black mb-5">Devis en attente de fin de contrat  </h2>
+			<table class=" table-auto m-auto">
+				<thead class="bg-white space-y-4  text-blue-600">
+					<tr>
+						<th>client</th>
+						<th>dénomination de la mission</th>
+						<th>montant HTVA</th>
+						<th>montant TVA</th>
+						<th>montant pour la coop</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>Interra</th>
+						<th>réalisation de leur site internet</th>
+						<th>3000</th>
+						<th>3000+21%</th>
+						<th>3000-6,5%</th>
+					</tr>
+				</tbody>
+	
+			</table>
+	
+		</article>
+		
+</main>
+
